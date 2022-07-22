024873f37f016b2ad426a0d1e00f1402dcf55359
cvvlm <michalsk7@gmail.com>
2022-07-22 17:37:10 +0200

    board size on user input changes
    Board size is now determined on user input and also add an indicator,
    that tells what is the current board size and if you overload it will
    tell you that it is too many squares.

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-let board = document.querySelector('.board');
-board.style.gridTemplateColumns = 'repeat(16 , 1fr)'
-board.style.gridTemplateRows = 'repeat(16 , 1fr)'
+let content = document.querySelector('.content');
+function populateBoard(size) {
+    let board = document.querySelector('.board');
+    let squares = board.querySelectorAll("div");
+    squares.forEach((div) => div.remove());
+    board.style.gridTemplateColumns = `repeat(${size} , 1fr)`
+    board.style.gridTemplateRows = `repeat(${size} , 1fr)`
+    let size2 = size * size;
 
-for (let i = 0; i < 256; i++) {
-    let square = document.createElement('div')
-    square.style.backgroundColor = 'blue'
-    board.appendChild(square)
-    
+    for (let i = 0; i < size2; i++) {
+       let square = document.createElement('div')
+       square.style.backgroundColor = 'blue'
+       board.appendChild(square)
+    }
+}
+populateBoard(16);
+
+function changeSize(input) {
+    let alert = document.querySelector('.board-size')
+    if (input >= 1 && input <= 100) {
+        populateBoard(input);
+        alert.textContent = `Board size: ${input}x${input}`
+    }
+    else {
+        alert.textContent = "too many squares";
+    }
 }
